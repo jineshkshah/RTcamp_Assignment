ba978081281eeb5cc5f71c1acdc9eb34d4e03cd0
jineshkshah <jineshkshah@users.noreply.github.com>
2016-09-22 15:28:13 +0530

    Add files via upload

---
+++ config.php
@@ -0,0 +1,5 @@
+<?php
+define('CONSUMER_KEY','Us1iCvcqvXT91aQK9WIEW1pvS');
+define('CONSUMER_SECRET','4CzUb4v8MNwy1vNNJeIjkeKlQhhRJPJ2bj4czD9VidkTqs6eVc');
+define('OAUTH_CALLBACK', 'http://rtassignment.azurewebsites.net/process.php');
+?>
---
+++ index.php
@@ -0,0 +1,91 @@
+<?php
+//start session
+session_start();
+include_once("config.php");
+include_once("include/twitteroauth.php");
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Login with Twitter using PHP</title>
+    <style type="text/css">
+	.wrapper{width:600px; margin-left:auto;margin-right:auto;}
+	.welcome_txt{
+		margin: 20px;
+		background-color: #EBEBEB;
+		padding: 10px;
+		border: #D6D6D6 solid 1px;
+		-moz-border-radius:5px;
+		-webkit-border-radius:5px;
+		border-radius:5px;
+	}
+	.tweet_box{
+		margin: 20px;
+		background-color: #FFF0DD;
+		padding: 10px;
+		border: #F7CFCF solid 1px;
+		-moz-border-radius:5px;
+		-webkit-border-radius:5px;
+		border-radius:5px;
+	}
+	.tweet_box textarea{
+		width: 500px;
+		border: #F7CFCF solid 1px;
+		-moz-border-radius:5px;
+		-webkit-border-radius:5px;
+		border-radius:5px;
+	}
+	.tweet_list{
+		margin: 20px;
+		padding:20px;
+		background-color: #E2FFF9;
+		border: #CBECCE solid 1px;
+		-moz-border-radius:5px;
+		-webkit-border-radius:5px;
+		border-radius:5px;
+	}
+	.tweet_list ul{
+		padding: 0px;
+		font-family: verdana;
+		font-size: 12px;
+		color: #5C5C5C;
+	}
+	.tweet_list li{
+		border-bottom: silver dashed 1px;
+		list-style: none;
+		padding: 5px;
+	}
+	</style>
+</head>
+<body>
+<?php
+	if(isset($_SESSION['status']) && $_SESSION['status'] == 'verified')
+	{
+		//Retrive variables
+		$screen_name 		= $_SESSION['request_vars']['screen_name'];
+		$twitter_id			= $_SESSION['request_vars']['user_id'];
+		$oauth_token 		= $_SESSION['request_vars']['oauth_token'];
+		$oauth_token_secret = $_SESSION['request_vars']['oauth_token_secret'];
+
+		//Show welcome message
+		echo '<div class="welcome_txt">Welcome <strong>'.$screen_name.'</strong> (Twitter ID : '.$twitter_id.'). <a href="logout.php?logout">Logout</a>!</div>';
+		$connection = new TwitterOAuth(CONSUMER_KEY, CONSUMER_SECRET, $oauth_token, $oauth_token_secret);
+
+		//If user wants to tweet using form.
+		if(isset($_POST["updateme"]))
+		{
+			//Post text to twitter
+			$my_update = $connection->post('statuses/update', array('status' => $_POST["updateme"]));
+			die('<script type="text/javascript">window.top.location="index.php"</script>'); //redirect back to index.php
+		}
+		echo '<a class="twitter-timeline" href="https://twitter.com/'.$screen_name.'">Tweets by '.$screen_name.'</a> <script async src="//platform.twitter.com/widgets.js" charset="utf-8" ></script>';s
+		echo '</ul></div>';
+
+	}else{
+		//Display login button
+		echo '<a href="process.php"><img src="images/sign-in-with-twitter.png" width="151" height="24" border="0" /></a>';
+	}
+?>
+</body>
+</html>
